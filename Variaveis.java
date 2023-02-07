394f2593df7f1287e2071f827387a1ad1584a120
João Gabriel <joao.silva2714@etec.sp.gov.br>
2023-02-07 15:17:27 +0000

    Salvando Arquivos

--- Variaveis.java
+++ Variaveis.java
@@ -1,3 +1,14 @@
 public class Variaveis {
-    
+    public static void main(String[] args) {
+        
+        int idade=30;
+        byte quantidadeBolas=7;
+        char letras='N';
+        boolean vivo=false;
+        double salario = 1302;
+        float salariomenor = 1212;
+        short alunoPalmital = 10000;
+        long populaçãoPalimatal= 22000;
+        System.out.println(idade);
+    } 
 }
