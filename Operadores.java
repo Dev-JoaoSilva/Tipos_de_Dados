7c12517e14187fcdd6bbafa75dad0973d092d70e
João Gabriel <joao.silva2714@etec.sp.gov.br>
2023-02-14 14:12:13 +0000

    envio via mensagem

--- Operadores.java
+++ Operadores.java
@@ -1,3 +1,15 @@
 public class Operadores {
-    
+    /**
+     * @param barela
+     */
+    public static void main(String[] barela){
+
+        int idade;
+        idade = 16;
+        idade++;
+        idade++;
+        System.out.println("Eu tenho "+ idade +");
+        idade+=10;
+        System.out.println(idade);
+    }
 }
