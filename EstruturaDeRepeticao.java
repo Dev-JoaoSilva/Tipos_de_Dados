74defe98f48cab06fbdbb252b7b721d5fd7a0782
João Gabriel <joao.silva2714@etec.sp.gov.br>
2023-02-15 16:41:37 +0000

    Exercício 1

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,3 +1,10 @@
 public class EstruturaDeRepeticao {
+    public static void main(String args[]) {
 
+        for(int i=150; i<=499;i++){
+            int soma = i + 1;
+            System.out.println(soma);
+        }
+
+    }
 }
