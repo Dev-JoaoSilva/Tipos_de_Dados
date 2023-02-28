aed5783eb41a7f03994623d68fac8188f4bc0337
João Gabriel <joao.silva2714@etec.sp.gov.br>
2023-02-28 14:23:35 +0000

    BalancoTrimestral

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -8,7 +8,7 @@ public class EstruturaDeRepeticao {
         }; EXERCÍCIO 1
 */
         int soma = 0;
-        for (int i=1; i<=1000; i++){
+        for (int i=1 -1; i<=1000; i++){
             soma = soma + i;
             System.out.println(soma);
         }
